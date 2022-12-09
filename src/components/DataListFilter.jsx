b2c805c755e642b71d38a6acf772dc926fed7783
Jose Gomez <jose@gtech.com>
2022-12-08 23:51:41 -0500

    filters in progress

--- src/components/DataListFilter.jsx
+++ src/components/DataListFilter.jsx
@@ -1,9 +1,56 @@
 import { useState } from "react";
 
+const StatusFilters = ({ statusFilters, setStatusFilter }) => {
+  return (
+    <>
+      {statusFilters.filters.map((filter, index) => (
+        <label className="dt-filters__status">
+          <input
+            checked={filter.active}
+            
+            type="checkbox"
+            name=""
+            id=""
+          />
+          <p className="text-m">
+            {filter.label} ({filter.count})
+          </p>
+        </label>
+      ))}
+    </>
+  );
+};
+
 export const DataListFilter = ({ filters }) => {
-  const [first, setFirst] = useState(null);
+  const [statusFilter, setStatusFilter] = useState({
+    filters: [
+      {
+        label: "All",
+        count: 500,
+        active: false,
+      },
+      {
+        label: "Open",
+        count: 30,
+        active: true,
+      },
+      {
+        label: "Completed",
+        count: 40,
+        active: false,
+      },
+      {
+        label: "Canceled",
+        count: 10,
+        active: false,
+      },
+    ],
+    active: false,
+  });
+
   return (
     <div className="dt-filters">
+      {JSON.stringify(statusFilter)}
       <div className="dt-filters__col">
         <div className="dt-filters__label">
           <img
@@ -13,18 +60,41 @@ export const DataListFilter = ({ filters }) => {
           />
           <h2 className="text-m-bold">Filters</h2>
         </div>
-        <div className="dt-filters-item">
-          <img
-            className="dt-filters-icon"
-            src="src/images/filter-icon.svg"
-            alt=""
-          />
-          <h2 className="text-m-bold">Order status</h2>
-          <img
-            className="dt-filters-arrow-icon"
-            src="src/images/filter-arrow.svg"
-            alt=""
-          />
+        <div
+          className={"dt-filters-item " + (statusFilter.active ? "active" : "")}
+        >
+          <div
+            className="dt-filters-item-cl"
+            onClick={() => {
+              setStatusFilter({
+                ...statusFilter,
+                active: !statusFilter.active,
+              });
+            }}
+          >
+            <img
+              className="dt-filters-icon"
+              src="src/images/filter-icon.svg"
+              alt=""
+            />
+            <h2 className="text-m-bold">Order status</h2>
+            <img
+              className="dt-filters-arrow-icon"
+              src="src/images/filter-arrow.svg"
+              alt=""
+            />
+          </div>
+          <div className="dt-filters__box">
+            <StatusFilters
+              statusFilters={statusFilter}
+              setStatusFilter={setStatusFilter}
+            />
+
+            <div className="dt-filters__box-buttons">
+              <button className="btn text-m-bold">Cancel</button>
+              <button className="btn btn-primary text-m-bold">Apply</button>
+            </div>
+          </div>
         </div>
         <div className="dt-filters-item">
           <img
@@ -56,12 +126,16 @@ export const DataListFilter = ({ filters }) => {
       </div>
       <div className="dt-filters__col">
         <label className="dt-filters__search">
-        <img
+          <img
             className="dt-filters-search-icon"
             src="src/images/search-icon.svg"
             alt=""
           />
-          <input placeholder="Search" className="dt-filters__search-input" type="text" />
+          <input
+            placeholder="Search"
+            className="dt-filters__search-input"
+            type="text"
+          />
         </label>
       </div>
     </div>
